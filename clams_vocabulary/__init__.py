e1dbdf82b708700e14f73090c0a9e86992cacb10
Keigh Rim <keigh.rim@gmail.com>
2026-03-13 18:51:04 -0400

    Build vocabulary 1.0.3
    Updated 11 types:
      - Annotation -> v4
      - BoundingBox -> v3
      - Chapter -> v4
      - Interval -> v3
      - Polygon -> v3
      - Region -> v3
      - Relation -> v3
      - Span -> v3
      - TimeFrame -> v4
      - TimePoint -> v3
      - VideoObject -> v3

--- clams_vocabulary/__init__.py
+++ clams_vocabulary/__init__.py
@@ -34,7 +34,7 @@ class Enums:
     """Namespace for all controlled vocabularies."""
     pass
 
-for _cls in [Alignment_v1, Alignment, Annotation_v2, Annotation_v3, Annotation, BoundingBox_v1, BoundingBox_v2, BoundingBox, Chapter_v1, Chapter_v2, Chapter_v3, Chapter, Interval_v1, Interval_v2, Interval, Polygon_v1, Polygon_v2, Polygon, Region_v1, Region_v2, Region, Relation_v1, Relation_v2, Relation, Span_v1, Span_v2, Span, Thing_v1, Thing, TimeFrame_v1, TimeFrame_v2, TimeFrame_v3, TimeFrame, TimePoint_v1, TimePoint_v2, TimePoint, VideoObject_v1, VideoObject_v2, VideoObject]:
+for _cls in [Alignment_v1, Alignment, Annotation_v2, Annotation_v3, Annotation_v4, Annotation, BoundingBox_v1, BoundingBox_v2, BoundingBox_v3, BoundingBox, Chapter_v1, Chapter_v2, Chapter_v3, Chapter_v4, Chapter, Interval_v1, Interval_v2, Interval_v3, Interval, Polygon_v1, Polygon_v2, Polygon_v3, Polygon, Region_v1, Region_v2, Region_v3, Region, Relation_v1, Relation_v2, Relation_v3, Relation, Span_v1, Span_v2, Span_v3, Span, Thing_v1, Thing, TimeFrame_v1, TimeFrame_v2, TimeFrame_v3, TimeFrame_v4, TimeFrame, TimePoint_v1, TimePoint_v2, TimePoint_v3, TimePoint, VideoObject_v1, VideoObject_v2, VideoObject_v3, VideoObject]:
     setattr(AnnotationTypes, _cls.__name__, _cls)
 
 for _cls in [AudioDocument_v1, AudioDocument, Document_v1, Document, ImageDocument_v1, ImageDocument, TextDocument_v1, TextDocument, VideoDocument_v1, VideoDocument]:
@@ -53,7 +53,7 @@ AnnotationTypes._typevers = {_t.shortname: _t.version for _t in [Alignment, Anno
 DocumentTypes._typevers = {_t.shortname: _t.version for _t in [AudioDocument, Document, ImageDocument, TextDocument, VideoDocument]}
 
 URI_TO_TYPE = {}
-for _type in [Alignment_v1, Alignment, Annotation_v2, Annotation_v3, Annotation, BoundingBox_v1, BoundingBox_v2, BoundingBox, Chapter_v1, Chapter_v2, Chapter_v3, Chapter, Interval_v1, Interval_v2, Interval, Polygon_v1, Polygon_v2, Polygon, Region_v1, Region_v2, Region, Relation_v1, Relation_v2, Relation, Span_v1, Span_v2, Span, Thing_v1, Thing, TimeFrame_v1, TimeFrame_v2, TimeFrame_v3, TimeFrame, TimePoint_v1, TimePoint_v2, TimePoint, VideoObject_v1, VideoObject_v2, VideoObject, AudioDocument_v1, AudioDocument, Document_v1, Document, ImageDocument_v1, ImageDocument, TextDocument_v1, TextDocument, VideoDocument_v1, VideoDocument]:
+for _type in [Alignment_v1, Alignment, Annotation_v2, Annotation_v3, Annotation_v4, Annotation, BoundingBox_v1, BoundingBox_v2, BoundingBox_v3, BoundingBox, Chapter_v1, Chapter_v2, Chapter_v3, Chapter_v4, Chapter, Interval_v1, Interval_v2, Interval_v3, Interval, Polygon_v1, Polygon_v2, Polygon_v3, Polygon, Region_v1, Region_v2, Region_v3, Region, Relation_v1, Relation_v2, Relation_v3, Relation, Span_v1, Span_v2, Span_v3, Span, Thing_v1, Thing, TimeFrame_v1, TimeFrame_v2, TimeFrame_v3, TimeFrame_v4, TimeFrame, TimePoint_v1, TimePoint_v2, TimePoint_v3, TimePoint, VideoObject_v1, VideoObject_v2, VideoObject_v3, VideoObject, AudioDocument_v1, AudioDocument, Document_v1, Document, ImageDocument_v1, ImageDocument, TextDocument_v1, TextDocument, VideoDocument_v1, VideoDocument]:
     URI_TO_TYPE[_type.uri] = _type
     for aka in _type.alsoKnownAs:
         URI_TO_TYPE[aka] = _type
--- clams_vocabulary/types/polygon/__init__.py
+++ clams_vocabulary/types/polygon/__init__.py
@@ -4,10 +4,11 @@ This file should never be manually modified, and must stay FROZEN.
 """
 from .v1 import Polygon_v1
 from .v2 import Polygon_v2
+from .v3 import Polygon_v3
 
 
-class Polygon(Polygon_v2):
-    """Latest version alias for Polygon_v2."""
+class Polygon(Polygon_v3):
+    """Latest version alias for Polygon_v3."""
     pass
 
-__all__ = ['Polygon_v1', 'Polygon_v2', 'Polygon']
+__all__ = ['Polygon_v1', 'Polygon_v2', 'Polygon_v3', 'Polygon']
