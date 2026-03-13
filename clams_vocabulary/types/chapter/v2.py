522b6b06f99cff7e146b2d72b3d837db4660a9a1
Keigh Rim <keigh.rim@gmail.com>
2026-03-13 17:59:46 -0400

    Build vocabulary 1.0.1
    Updated 2 types:
      - Chapter -> v2
      - TimeFrame -> v2

--- clams_vocabulary/types/chapter/v2.py
+++ clams_vocabulary/types/chapter/v2.py
@@ -1,9 +1,17 @@
+"""
+This file is auto-generated.
+This file should never be manually modified, and must stay FROZEN.
+"""
 from typing import ClassVar, Optional
 from pydantic import Field
-from clams_vocabulary.types.time_frame.archetype import TimeFrame
+from clams_vocabulary.types.time_frame.v2 import TimeFrame_v2
 
 
-class Chapter(TimeFrame):
+class Chapter_v2(TimeFrame_v2):
+    uri: ClassVar[str] = "http://clams.ai/vocabulary/type/Chapter/v2"
+    version: ClassVar[str] = "v2"
+    shortname: ClassVar[str] = "Chapter"
+
     description: ClassVar[str] = (
         "Example case for when we do not want to use Segment with a specific segmentType or if we want to introduce "
         "special properties."
--- clams_vocabulary/types/time_frame/__init__.py
+++ clams_vocabulary/types/time_frame/__init__.py
@@ -3,10 +3,11 @@ This file is auto-generated.
 This file should never be manually modified, and must stay FROZEN.
 """
 from .v1 import TimeFrame_v1
+from .v2 import TimeFrame_v2
 
 
-class TimeFrame(TimeFrame_v1):
-    """Latest version alias for TimeFrame_v1."""
+class TimeFrame(TimeFrame_v2):
+    """Latest version alias for TimeFrame_v2."""
     pass
 
-__all__ = ['TimeFrame_v1', 'TimeFrame']
+__all__ = ['TimeFrame_v1', 'TimeFrame_v2', 'TimeFrame']
